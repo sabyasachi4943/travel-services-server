fdad9af0e6e244a40d6da39789e5a6090d4a7deb
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-11-09 14:43:54 +0600

    service-review sorted

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,10 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hiw3ljn.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hiw3ljn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,7 +18,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const serviceCollection = client.db("travel-services").collection("services");
+    const serviceCollection = client
+      .db("travel-services")
+      .collection("services");
     const reviewCollection = client.db("travel-services").collection("reviews");
 
     app.get("/services", async (req, res) => {
@@ -35,41 +35,56 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
       res.send(service);
-    })
+    });
 
     // reviews api
-    app.post('/reviews', async (req, res) => {
+    app.post("/reviews", async (req, res) => {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
       res.send(result);
-    })
+    });
 
-    app.get('/reviews', async (req, res) => {
+    app.get("/reviews", async (req, res) => {
       let query = {};
 
       if (req.query.email) {
         query = {
-          email: req.query.email
-        }
+          email: req.query.email,
+        };
       }
       const cursor = reviewCollection.find(query);
       const reviews = await cursor.toArray();
       res.send(reviews);
-    })
+    });
+
+    app.get("/service-review", async (req, res) => {
+      let query = {};
+      if (req.query.service) {
+        query = {
+          service: req.query.service,
+        };
+      }
+      const options = {
+        sort:{
+          "createdAt": -1
+        }
+    };
+      const cursor = reviewCollection.find(query, options);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
 
 
-  }
-  finally {
-    
+  } finally {
   }
 }
 
-run().catch(err => console.error(err));
+run().catch((err) => console.error(err));
 
-app.get('/', (req, res) => {
-  res.send('travel services server is running')
-})
+app.get("/", (req, res) => {
+  res.send("travel services server is running");
+});
 
 app.listen(port, () => {
   console.log(`travel services server running on ${port}`);
-})
+});
