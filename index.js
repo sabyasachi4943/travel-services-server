dacbe6d511979ebf977eac60a0824a027e7581fa
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-11-09 14:30:37 +0600

    getting reviews

--- index.js
+++ index.js
@@ -43,6 +43,21 @@ async function run() {
       const result = await reviewCollection.insertOne(review);
       res.send(result);
     })
+
+    app.get('/reviews', async (req, res) => {
+      let query = {};
+
+      if (req.query.email) {
+        query = {
+          email: req.query.email
+        }
+      }
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    })
+
+
   }
   finally {
     
