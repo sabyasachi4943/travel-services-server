a33fa140b49110926b71901a24ab9b41c22dfa72
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-11-10 22:27:21 +0600

    vercel

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId, ObjectID } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hiw3ljn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -57,6 +57,28 @@ async function run() {
       const reviews = await cursor.toArray();
       res.send(reviews);
     });
+
+    app.delete('/reviews/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.patch('/reviews/:id', async (req, res) => {
+      console.log("hit")
+      const id = req.params.id;
+      const text = req.body.text;
+      console.log(id, text);
+      const query = { _id: ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          text: text
+        }
+      }
+      const result = await reviewCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
 
     app.get("/service-review", async (req, res) => {
       let query = {};
